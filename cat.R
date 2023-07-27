df9c9284786b18e0443a612b90850c0080aaa8fc
gomba <jujmdu@gmail.com>
2023-07-27 11:16:34 +0200

    Update cat.R
    add model

--- cat.R
+++ cat.R
@@ -10,7 +10,7 @@
 # 7) Data splitting (Training and Test datasets)
 # 8) Model building using Training dataset 
 
-## 8.1) using catboost and analyze feature importance 
+## 8.1) #using catboost and analyze feature importance 
 ## 8.2) using factor analysis and analyze factor importance 
 
 # 9) Test your model performance on Test dataset
@@ -75,5 +75,61 @@ SmartEDA::ExpData(customers, type=2)
 
 # 5. Filling missing values
 DF[is.na(DF)] <- 0 
+
+
+###############
+# 7 & 8
+## catboost
+###############
+## https://stackoverflow.com/questions/60440590/r-catboost-to-handle-categorical-variables
+
+library(caret)
+library(catboost)
+library(mlbench)
+data(DF)
+#create train and test data sets:
+
+set.seed(1)
+
+tr <- createDataPartition(DF$Class, p = 0.7, list = FALSE)
+
+trainer <- DF[tr,]
+tester <- DF[-tr,]
+#train models:
+
+fitControl <- trainControl(method = "cv",
+                           number = 3,
+                           savePredictions = TRUE,
+                           summaryFunction = twoClassSummary,
+                           classProbs = TRUE)
+
+model <- train(x = trainer[,1:60],
+               y = trainer$Class,
+               method = catboost.caret, 
+               trControl = fitControl, 
+               tuneLength = 5,
+               metric = "ROC")
+#predict using caret:
+
+preds1 <- predict(model, tester, type = "prob")
+# save the final model:
+
+catboost::catboost.save_model(model$finalModel, "model")
+#load the saved model:
+
+model2 <- catboost::catboost.load_model("model")
+#predict using the saved model:
+
+preds2 <- catboost.predict(model2,
+                           catboost.load_pool(tester),
+                           prediction_type = "Probability")
+#check equality of predictions
+
+all.equal(preds1[,2], preds2)
+
+
+saveRDS(model, "caret.model.rds")
+model3 <- readRDS("caret.model.rds")
+preds3 <- predict(model3, tester, type = "prob")
 
 
